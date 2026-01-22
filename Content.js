540e1c300b57cec734a8cdac711fbcceff912bcd
baskar <baskar63699@gmail.com>
2026-01-22 17:31:10 +0530

    useState

--- Content.js
+++ Content.js
@@ -2,21 +2,18 @@ import React from 'react'
 import { useState } from "react";
 
 const Content = () => {
-
-    function handleNameChange() {
+      const [name, setName] = useState("TVK");
+      function handleNameChange() {
       const names = ["Master", "Leo", "GOAT", "Jana Nayakan", "TVK"];
       const int = [Math.floor(Math.random() * 5)];
-      return names[int];
-    }
-
-    function handleClick(name) {
-      alert(`Welcome to Vijay's world! ${name}`);
-    }
+      setName(names[int]);
+    } 
+    
   return (
     <main>
-      <p>Vijay's {handleNameChange()}</p>
-      <button className="btn" onClick={()=> handleClick('TVK')}> Join </button>
-    </main>
+      <p>Vijay's {name} World!</p>
+      <button className="btn" onClick={handleNameChange}> Join </button>
+     </main>
   );
 }
 
